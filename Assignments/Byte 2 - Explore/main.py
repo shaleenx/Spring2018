5fd9ca3115d7f40c5c11a33ad50dee4ba7a3e997
shaleenx <shaleenx@gmail.com>
2018-10-17 22:52:45 -0400

    Add Byte 4b

--- Assignments/Byte 2 - Explore/main.py
+++ Assignments/Byte 2 - Explore/main.py
@@ -24,18 +24,20 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     autoescape=True)
 
 # This API key is provided by google as described in the tutorial
-API_KEY = 'XXxxXxXXXXxxNXXxXXXxxxNNXXxxxxxxxXXXxXX'
+API_KEY = 'AIzaSyBThkmyl1thVKZDE47pOr48FezGaKXP6dY'
 
 # This uses discovery to create an object that can talk to the 
 # fusion tables API using the developer key
 service = build('fusiontables', 'v1', developerKey=API_KEY)
 
 # This is the table id for the fusion table
-TABLE_ID = 'NxxxNXxXxxNxXXXXNXxXXXxXxxxNxXxNxXxxXxxX'
+TABLE_ID = '1z6rbsoHNdeUGz-sQOcj8KGgZvoXTQYjro9gGK4Xd'
+#TABLE_ID = '1VlPiBCkYt_Vio-JT3UwM-U__APurJvPb6ZEJPg'
 
 # This is the default columns for the query
 query_cols = []
-query_values = ['Forlan'] #Change to be the value(s) you're querying in the column you've specified
+#query_values = ['Forlan'] #Change to be the value(s) you're querying in the column you've specified
+query_values = ['100'] #Change to be the value(s) you're querying in the column you've specified
 
 # Import the Flask Framework
 from flask import Flask, request
@@ -71,7 +73,8 @@ def make_query(cols, values, limit):
     string_values = string_values[2:len(string_values)]
     
     #Change this query to have your corresponding column (in our soccer example, the column for our WHERE is Scorer).
-    query = "SELECT " + string_cols + " FROM " + TABLE_ID + " WHERE Scorer = '" + string_values + "'"
+    #query = "SELECT " + string_cols + " FROM " + TABLE_ID + " WHERE Scorer = '" + string_values + "'"
+    query = "SELECT " + string_cols + " FROM " + TABLE_ID + " WHERE Races > '" + string_values + "'"
 
     query = query + " LIMIT " + str(limit)
 
